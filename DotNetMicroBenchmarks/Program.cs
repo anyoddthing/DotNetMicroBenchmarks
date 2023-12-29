085abaeb91d47560d32af9d8a83abb8d8cf3d918
Daniel Doubleday <daniel.doubleday@gmail.com>
2023-12-29 23:38:51 +0100

    Dictionary vs SortedList

--- DotNetMicroBenchmarks/Program.cs
+++ DotNetMicroBenchmarks/Program.cs
@@ -1,5 +1,4 @@
 ﻿using DotNetMicroBenchmarks.Benchmarks;
-using DotNetMicroBenchmarks.Benchmarks.Boxing;
 
 namespace DotNetMicroBenchmarks;
 
@@ -7,12 +6,6 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        // Runner.Run<Benchmarks.Boxing.CovariantPrimitiveBoxing>();
-        Console.WriteLine(1.Intern().Equals(1));
-        Console.WriteLine(99.Intern().Equals(99));
-
-        Console.WriteLine(ReferenceEquals(57.Intern(), 57.Intern()));
-
-        Console.WriteLine((int) 27.Intern());
+        Runner.Run<Benchmarks.Boxing.IntCache.Benchmark>();
     }
 }
